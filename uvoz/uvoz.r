e6cb31050b3341504b9aa36ac145cda5c0b2a42e
Tjaša Trafela <tjasa.trafela@student.fmf.uni-lj.si>
2019-12-14 16:19:34 +0100

    vaja

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,8 +1,10 @@
 library(dplyr)
 library(tidyr)
 library(readr)
-uvoz1<-read_csv("povprecni_pridelek.csv", locale=locale(encoding="Windows-1252"))
-UVOZ2<-read_csv("podatki/zivina.csv", locale=locale(encoding="Windows-1250"))
-uvoz3<-read_csv("podatki/cene.csv", locale=locale(encoding="Windows-1250")) 
+library(tibble)
+uvoz1<-read_csv("povprecni_pridelek.csv", locale=locale(encoding="Windows-1250"))
+UVOZ2<-read_csv("zivina.csv", locale=locale(encoding="Windows-1250"))
+uvoz3<-read_csv("cene.csv", locale=locale(encoding="Windows-1250"))
 
-                 
+
+     
