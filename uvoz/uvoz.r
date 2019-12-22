976e2c565054e06ad246f121a737603115b2cbae
Tjaša Trafela <tjasa.trafela@student.fmf.uni-lj.si>
2019-12-22 16:03:38 +0100

    popravek

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -2,9 +2,22 @@ library(dplyr)
 library(tidyr)
 library(readr)
 library(tibble)
-uvoz1<-read_csv("povprecni_pridelek.csv", locale=locale(encoding="Windows-1250"))
-UVOZ2<-read_csv("zivina.csv", locale=locale(encoding="Windows-1250"))
-uvoz3<-read_csv("cene.csv", locale=locale(encoding="Windows-1250"))
 
 
-     
+
+uvoz1 <- read_csv("podatki/povprecni_pridelek.csv",
+                  locale=locale(encoding="Windows-1250"), na="-") %>%
+  rename(kmetijska.kultura=`KMETIJSKE KULTURE`) %>% # presledki so nezaželeni v imenih stolpcev
+  gather("leto.regija", "kolicina", -kmetijska.kultura, na.rm=TRUE) %>% # pretvorba v tidy data
+  separate(leto.regija, c("leto", "regija"), "(?<=[0-9]) ") %>% # delimo po presledku za številko
+  mutate(leto=parse_number(leto)) # leto pretvorimo v števila
+uvoz2 <- read_csv("podatki/zivina.csv", # v enem od stolpcev ni podatkov, zato bo treba eksplicitno podati tipe:
+                  col_types=cols(`VRSTA ŽIVINE`=col_character(), .default=col_integer()),
+                  locale=locale(encoding="Windows-1250"), na=c("N", "z"))  %>%
+  rename(vrsta.zivine=`VRSTA ŽIVINE`) %>%
+  gather("leto.regija", "stevilo", -vrsta.zivine, na.rm=TRUE) %>%
+  separate(leto.regija, c("leto", "regija"),
+           " (?=[A-ZČŠŽ])(?!Slovenija)") %>% # delimo po presledku pred veliko črko, razen pred besedo "Slovenija"
+  mutate(leto=parse_number(leto))
+
+
